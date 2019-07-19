a557b7df32e882f2cd0a6f13aa002b1901571995
CodeRTS68 <coderts301068@gmail.com>
2019-07-19 23:49:45 +0700

    first redux thunk

---
+++ src/reducers/index.js
@@ -0,0 +1,13 @@
+import {combineReducers} from 'redux';
+import postsReducer from './postsReducer';
+import usersReducer from './usersReducer';
+ 
+
+
+
+
+
+export default combineReducers({
+    posts: postsReducer,
+    users: usersReducer
+});
